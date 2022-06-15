a66cc1eb94bda68edec9c4beab2c8cf2efce77bb
Santiago De Olivera <santiagodeolivera@gmail.com>
2022-06-15 20:20:49 -0300

    UT8_PD1

---
+++ UT8_TAs/UT8_PD1/src/main/java/ut8/ut8_pd1/Tupla.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ut8.ut8_pd1;
+
+import java.util.Objects;
+
+public class Tupla<T, U> {
+    public T primero;
+    public U segundo;
+    
+    public Tupla(T primero, U segundo) {
+        this.primero = primero;
+        this.segundo = segundo;
+    }
+    
+    @Override
+    public boolean equals(Object other) {
+        if (other instanceof Tupla tupla) {
+            return this.primero.equals(tupla.primero) && this.segundo.equals(tupla.segundo);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 67 * hash + Objects.hashCode(this.primero);
+        hash = 67 * hash + Objects.hashCode(this.segundo);
+        return hash;
+    }
+    
+}
