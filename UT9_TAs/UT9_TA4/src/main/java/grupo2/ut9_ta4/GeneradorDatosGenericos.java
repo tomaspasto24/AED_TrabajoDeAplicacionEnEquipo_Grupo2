d087fa594a07775f55c129452eff56783921aadb
Santiago De Olivera <santiagodeolivera@gmail.com>
2022-06-30 09:58:38 -0300

    .

--- UT9_TAs/UT9_TA4/src/main/java/grupo2/ut9_ta4/GeneradorDatosGenericos.java
+++ UT9_TAs/UT9_TA4/src/main/java/grupo2/ut9_ta4/GeneradorDatosGenericos.java
@@ -4,14 +4,18 @@ import java.util.Random;
 
 public class GeneradorDatosGenericos {
 	private static int TAMANIO_MAX = 32000;
+        
 	public int[] generarDatosAleatorios() {
+            return generarDatosAleatorios(TAMANIO_MAX);
+        }
+	public int[] generarDatosAleatorios(int tamanioMax) {
 		Random rnd = new Random();
-		int[] datosGenerados = new int[TAMANIO_MAX];
-		boolean[] datosUtilizados = new boolean[TAMANIO_MAX];
+		int[] datosGenerados = new int[tamanioMax];
+		boolean[] datosUtilizados = new boolean[tamanioMax];
 		for (int i = 0; i < datosGenerados.length; i++) {
-			int j = rnd.nextInt(TAMANIO_MAX);
+			int j = rnd.nextInt(tamanioMax);
 			while(datosUtilizados[j]){
-				j = (j + 1) % TAMANIO_MAX;
+				j = (j + 1) % tamanioMax;
 			}
 			datosGenerados[j] = i;
 			datosUtilizados[j] = true;
@@ -20,17 +24,23 @@ public class GeneradorDatosGenericos {
 	}
 	
 	public int[] generarDatosAscendentes() {
-		int [] copiaAscendente = new int[TAMANIO_MAX];
-		for (int i = 0; i < TAMANIO_MAX; i++) {
+            return generarDatosAscendentes(TAMANIO_MAX);
+        }
+	public int[] generarDatosAscendentes(int tamanioMax) {
+		int [] copiaAscendente = new int[tamanioMax];
+		for (int i = 0; i < tamanioMax; i++) {
 			copiaAscendente[i] = i;
 		}
 		return copiaAscendente;
 	}
 	
 	public int[] generarDatosDescendentes() {
-		int [] copiaDescendente = new int[TAMANIO_MAX];
-		for (int i = 0; i < TAMANIO_MAX; i++) {
-			copiaDescendente[i] = TAMANIO_MAX - i;
+            return generarDatosDescendentes(TAMANIO_MAX);
+        }
+	public int[] generarDatosDescendentes(int tamanioMax) {
+		int [] copiaDescendente = new int[tamanioMax];
+		for (int i = 0; i < tamanioMax; i++) {
+			copiaDescendente[i] = tamanioMax - i;
 		}
 		return copiaDescendente;
 	}
--- UT9_TAs/UT9_TA4/src/main/java/grupo2/ut9_ta4/UT9_TA6.java
+++ UT9_TAs/UT9_TA4/src/main/java/grupo2/ut9_ta4/UT9_TA6.java
@@ -4,12 +4,20 @@
  */
 package grupo2.ut9_ta4;
 
+import java.util.Arrays;
+
 /**
  *
  * @author santi
  */
 public class UT9_TA6 {
     public static void main(String[] args) {
+        TClasificador clasif = new TClasificador();
+        GeneradorDatosGenericos generador = new GeneradorDatosGenericos();
         
+        int[] vector = generador.generarDatosAscendentes(10);
+        clasif.clasificar(vector, TClasificador.METODO_CLASIFICACION_HEAPSORT);
+        System.out.println(clasif.estaOrdenado(vector));
+        System.out.println(Arrays.toString(vector));
     }
 }
